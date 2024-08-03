2baa158541e9893f745cae659c8aa07f5a851ef0
Nabil Kriouane <96531078+bjrdah@users.noreply.github.com>
2024-08-03 08:20:25 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tables</title>
+</head>
+<body>
+<form action="">
+    <table border="1">
+        <tr>
+            <td>nom : </td>
+            <td><input type="text"></td>
+        </tr>
+        <tr>
+            <td>vous aimez</td>
+            <td>ENG<input type="checkbox" name="" id="">FRA <input type="checkbox" name="" id=""></td>
+        </tr>
+    </table>
+</form>
+    
+</body>
+</html>
